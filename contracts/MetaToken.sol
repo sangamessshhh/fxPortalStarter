2f0071aa7b6236985eced6a169e150316a75e626
sangamessshhh <138979376+sangamessshhh@users.noreply.github.com>
2024-03-03 20:49:58 +0530

    Update MetaToken.sol

--- contracts/MetaToken.sol
+++ contracts/MetaToken.sol
@@ -1,17 +1,40 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "erc721a/contracts/ERC721A.sol";
 
-contract MetaToken is ERC20, Ownable {
-    constructor() ERC20("MetaToken", "MTA") {}
+contract Ferrari is ERC721A{
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    address public owner;
+    uint256 public maxNFTs;
+    string baseUrl = "https://gateway.pinata.cloud/ipfs/Qme2LahM9DpQvD7zu8hgezdzgL1dP6csesHxKUVamWPkAG";
+
+    string public input_prompt;
+
+    constructor() ERC721A("Ferrari", "FR") {
+        owner = msg.sender;
+        maxNFTs = 5;
+        input_prompt ="robot driving a ferrari";
+        
     }
 
-    function decimals() public pure override returns (uint8) {
-		return 0;
-	}
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function mint(uint256 input_NFTs) external payable onlyOwner{
+        require(totalSupply() + input_NFTs <= maxNFTs ,"Only 5 Allowed!");
+        _mint(msg.sender, input_NFTs);
+    }
+
+
+    function _baseURI() internal view override returns (string memory){
+        return baseUrl;
+    }
+
+    function promptDetails() external view returns (string memory) {
+        return input_prompt;
+    }
 }
